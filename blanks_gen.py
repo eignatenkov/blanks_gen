50c49bc198caf2f781328c06f3ff5bffc9822bd9
Egor Ignatenkov <egor.ignatenkov@gmail.com>
2018-11-19 22:21:00 +0100

    first commit

---
+++ blanks_gen.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+#! -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from __future__ import division
+import pdb
+import sys
+import os
+import codecs
+import subprocess
+import shlex
+import shutil
+import argparse
+try:
+    unicode
+except NameError:
+    unicode = str
+
+
+def make_filename(filename, i, maxi):
+    basename = os.path.splitext(filename)[0]
+    ext = os.path.splitext(filename)[1]
+    zf = len(unicode(maxi))
+    return basename + '_{}'.format(unicode(i).zfill(zf)) + ext
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('teams', type=int)
+    parser.add_argument('--addteams')
+    parser.add_argument('--blanks', default='blanks.tex')
+    parser.add_argument('--placeholder', default='\hrulefill')
+    parser.add_argument('--logs', action='store_true')
+    parser.add_argument('--output_dir', default=None)
+    args = parser.parse_args()
+
+    blueprint = codecs.open(args.blanks, 'r', 'utf8').read()
+
+    if args.output_dir:
+        shutil.copy(args.blanks, args.output_dir)
+        shutil.copy('blanksheader.tex', args.output_dir)
+        os.chdir(args.output_dir)
+
+    numbers = list(range(1, args.teams + 1))
+    if args.addteams:
+        numbers = numbers + [int(x) for x in args.addteams.split(',')]
+
+    new_filenames = []
+    for i in numbers:
+        nf = make_filename(args.blanks, i, max(numbers))
+        new_filenames.append(nf)
+        print('processing {}'.format(nf))
+        with codecs.open(nf, 'w', 'utf8') as f:
+            f.write(blueprint.replace(
+                args.placeholder, unicode(i) + '\\hfill'
+            ))
+        subprocess.call(
+            shlex.split(
+                'xelatex -synctex=1 -interaction=nonstopmode "{}"'.format(nf)),
+            stdout=open(os.devnull, 'wb')
+        )
+    for x in new_filenames:
+        os.remove(x)
+    if not args.logs:
+        for x in os.listdir(os.getcwd()):
+            if x.endswith(('.log', '.aux', '.out', '.gz')):
+                os.remove(x)
+
+
+if __name__ == "__main__":
+    main()
